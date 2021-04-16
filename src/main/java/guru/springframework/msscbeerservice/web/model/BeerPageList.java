7ae425fcc98a3158789a359debfd85be90b24040
Alena Dziomina <alenadziomina@gmail.com>
2021-04-16 14:52:23 +0200

    Model impl

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPageList.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPageList extends PageImpl<BeerDto> {
+    public BeerPageList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package guru.springframework.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, ALE, STOUT, GOSE, IPA
+}

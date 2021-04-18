cd8ae52ccffd5a89199077cef27ee3e348d5d3d3
Alena Dziomina <alenadziomina@gmail.com>
2021-04-19 00:53:22 +0200

    Update spring boot version and register in eureka

---
+++ src/main/java/guru/springframework/msscbeerservice/web/config/LocalDiscovery.java
@@ -0,0 +1,11 @@
+package guru.springframework.msscbeerservice.web.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscovery {
+}
